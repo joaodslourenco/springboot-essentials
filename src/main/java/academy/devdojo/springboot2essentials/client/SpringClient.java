8a638cd3c7ae335d5ee2ca16f5ac6173e67f8c61
João Lourenço <joaodslourenco@gmail.com>
2024-06-17 19:22:18 -0300

    feat: add post and put methods at client

--- src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -2,20 +2,50 @@ package academy.devdojo.springboot2essentials.client;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        String url = "http://localhost:8080/animes/{id}";
-
-        ResponseEntity<Anime> entity = new RestTemplate().getForEntity(url, Anime.class, 2);
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(entity);
 
 
-        Anime anime = new RestTemplate().getForObject(url, Anime.class, 1);
+        Anime[] anime = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
 
-        log.info(anime);
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {
+        });
+
+        log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+//        log.info("Saved anime: {}", kingdomSaved);
+
+        Anime samurai = Anime.builder().name("Samurai Champloo").build();
+
+        ResponseEntity<Anime> samuraiSaved = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST, new HttpEntity<>(samurai, createJsonHeader()), new ParameterizedTypeReference<Anime>() {
+        });
+        log.info("Saved anime: {}", samuraiSaved);
+
+        
+        Anime animeToBeUpdated = samuraiSaved.getBody();
+
+        animeToBeUpdated.setName("Samurai Champloo 2");
+
+
+        ResponseEntity<Void> samuraiUpdated = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.PUT, new HttpEntity<>(samurai, createJsonHeader()), Void.class);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
